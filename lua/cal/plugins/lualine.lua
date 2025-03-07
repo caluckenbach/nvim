2736bcfcb27e2a59ca3b7818a1b3624e9eec0ca9
luckenco <cluckenbach@protonmail.com>
2025-03-07 11:15:51 +0100

    add lualine

---
+++ lua/cal/plugins/lualine.lua
@@ -0,0 +1,48 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local lualine = require("lualine")
+    lualine.setup({
+      options = {
+        icons_enabled = true,
+        theme = "auto",
+        component_separators = "",
+        section_separators = { left = "", right = "" },
+        disabled_filetypes = {
+          statusline = {},
+          winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        always_show_tabline = true,
+        globalstatus = false,
+        refresh = {
+          statusline = 100,
+          tabline = 100,
+          winbar = 100,
+        },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff" },
+        lualine_c = { "diagnostics" },
+        lualine_x = { "filetype" },
+        lualine_y = {},
+        lualine_z = { "filename", "location" },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {},
+      winbar = {},
+      inactive_winbar = {},
+      extensions = {},
+    })
+  end,
+}
