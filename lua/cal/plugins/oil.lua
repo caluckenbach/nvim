355d136bfff385cbb35085bccdf0f882500edf07
Constantin Luckenbach <cluckenbach@protonmail.com>
2026-08-07 16:39:22 +0200

    feat: add Oil parent directory shortcut

--- lua/cal/plugins/oil.lua
+++ lua/cal/plugins/oil.lua
@@ -3,9 +3,9 @@ return {
   ---@module 'oil'
   ---@type oil.SetupOpts
   opts = {},
-  -- Optional dependencies
-  -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  keys = {
+    { "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
+  },
   lazy = false,
 }
