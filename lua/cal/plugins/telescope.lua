7c54e4ea5ad4ff443d2c302f2d5b3a8bf9acec4a
Constantin Luckenbach <cluckenbach@protonmail.com>
2026-08-07 16:39:37 +0200

    chore: remove dead config scaffolding

--- lua/cal/plugins/telescope.lua
+++ lua/cal/plugins/telescope.lua
@@ -5,34 +5,15 @@ return {
     "nvim-lua/plenary.nvim",
     {
       "nvim-telescope/telescope-fzf-native.nvim",
-      -- Runs once when plugin is istalled/updated
       build = "make",
-
       cond = function()
         return vim.fn.executable("make") == 1
       end,
     },
     { "nvim-telescope/telescope-ui-select.nvim" },
-
-    -- Only work with a nerd font.
-    -- { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
   },
   config = function()
-    -- Two important keymaps to use while in Telescope are:
-    --  - Insert mode: <c-/>
-    --  - Normal mode: ?
-
-    -- See `:help telescope` and `:help telescope.setup()`
     require("telescope").setup({
-      -- You can put your default mappings / updates / etc. in here
-      --  All the info you're looking for is in `:help telescope.setup()`
-      --
-      -- defaults = {
-      --   mappings = {
-      --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-      --   },
-      -- },
-      -- pickers = {}
       extensions = {
         ["ui-select"] = {
           require("telescope.themes").get_dropdown(),
@@ -40,11 +21,9 @@ return {
       },
     })
 
-    -- Enable Telescope extensions if they are installed
     pcall(require("telescope").load_extension, "fzf")
     pcall(require("telescope").load_extension, "ui-select")
 
-    -- See `:help telescope.builtin`
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "Search Help" })
     vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "Search Keymaps" })
@@ -57,17 +36,13 @@ return {
     vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Search by Grep" })
     vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Find existing buffers" })
 
-    -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set("n", "<leader>/", function()
-      -- You can pass additional configuration to Telescope to change the theme, layout, etc.
       builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
         winblend = 10,
         previewer = false,
       }))
     end, { desc = "Fuzzily search in current buffer" })
 
-    -- It's also possible to pass additional configuration options.
-    --  See `:help telescope.builtin.live_grep()` for information about particular keys
     vim.keymap.set("n", "<leader>s/", function()
       builtin.live_grep({
         grep_open_files = true,
@@ -75,7 +50,6 @@ return {
       })
     end, { desc = "Search / in Open Files" })
 
-    -- Shortcut for searching your Neovim configuration files
     vim.keymap.set("n", "<leader>sn", function()
       builtin.find_files({ cwd = vim.fn.stdpath("config") })
     end, { desc = "[S]earch [N]eovim files" })
